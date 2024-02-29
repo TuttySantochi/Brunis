c7a8a33c2f17b1843ed2050b8cb3b1a3c0efaf68
Patricio Sirena <patobsirena@gmail.com>
2024-02-28 22:57:14 -0300

    Agrego modelos de trabajos y contactos

--- src/app/services/works.service.ts
+++ src/app/services/works.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http' 
+import {Work} from '../models/work'
+import {Observable} from 'rxjs'
+
+const url = "http://localhost:3000/clients"
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorksService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  wroks: object[] = [
-    {ubicacion: "Hipolito Yrigoyen 207"
+  getWorks () : Observable<Work[]>{
+    return this.http.get<Work[]>(url)
   }
-  ]
+
+  getWork(id: number): Observable<Work> {
+    return this.http.get<Work>(`${url}/${id}`);
+  }
+
+  addWork(work: Work) {
+    return this.http.post(url, work);
+  }
+
+  updateWork(id: number, work: Work) {
+    return this.http.put(`${url}/${id}`, work);
+  }
+
+  deleteWork(id: number) {
+    return this.http.delete(`${url}/${id}`);
+  }
+
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -3,31 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
   cards = [
-    {nombre: 'May Toscano',
-    tipo: 'Cocina',
-    color: 'Grafito, Helsinki', 
-    imagen: 'assets/MayToscano.jpeg'},
-    {nombre: 'Rosa Silberstein',
-    tipo: 'Escritorio',
-    color: 'Camelia, Grafito',
-    imagen: 'assets/RositaSilberstein.jpeg'},
-    {nombre: 'Mariano Torres',
-    tipo: 'Vinoteca',
-    color: 'Lino Negro',
-    imagen: 'assets/MarianoTorres.jpg'},
-    {nombre: 'Nadia Atenor',
-    tipo: 'Rack TV',
-    color: 'Blanco, Concreto',
-    imagen: 'assets/NadiaAtenor.jpeg'},
-    {nombre: 'Nacho Arbeloa',
-    tipo: 'Cama Montessori',
-    color: 'Seda Giorno',
-    imagen: 'assets/NachoArbeloa.jpeg'}
-  ]
- 
+    {
+      nombre: 'May Toscano',
+      tipo: 'Cocina',
+      color: 'Grafito, Helsinki',
+      imagen: 'assets/MayToscano.jpeg',
+    },
+    {
+      nombre: 'Rosa Silberstein',
+      tipo: 'Escritorio',
+      color: 'Camelia, Grafito',
+      imagen: 'assets/RositaSilberstein.jpeg',
+    },
+    {
+      nombre: 'Mariano Torres',
+      tipo: 'Vinoteca',
+      color: 'Lino Negro',
+      imagen: 'assets/MarianoTorres.jpg',
+    },
+    {
+      nombre: 'Nadia Atenor',
+      tipo: 'Rack TV',
+      color: 'Blanco, Concreto',
+      imagen: 'assets/NadiaAtenor.jpeg',
+    },
+    {
+      nombre: 'Nacho Arbeloa',
+      tipo: 'Cama Montessori',
+      color: 'Seda Giorno',
+      imagen: 'assets/NachoArbeloa.jpeg',
+    },
+  ];
 }
