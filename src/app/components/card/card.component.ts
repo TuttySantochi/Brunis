25eddfbd8da5f72cbb890694952604e26c6d41f7
Patricio Sirena <patobsirena@gmail.com>
2024-03-08 12:12:00 -0300

    Soluciono error al eliminar trabajo

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,61 +1,76 @@
 import { Component, Input } from '@angular/core';
-import {WorksService} from '../../services/works.service'
-import {Work} from '../../models/work'
+import { WorksService } from '../../services/works.service';
+import { Work } from '../../models/work';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-
   work: Work = {
-      id: '',
-      clientName: '',
-      furnitureType: '',
-      furnitureColor: '',
-      location: '',
-      type: '',
-      elevator: '',
-      doors: '',
-      walls: '',
-      pipes: '',
-      wiring: '',
-      plinth: false,
-      bench:  false,
-      plugs: false,
-      corbel: false,
-      picture: '',
-      notes: ''
-  }
-  
+    id: '',
+    clientName: '',
+    furnitureType: '',
+    furnitureColor: '',
+    location: '',
+    type: '',
+    elevator: '',
+    doors: '',
+    walls: '',
+    pipes: '',
+    wiring: '',
+    plinth: false,
+    bench: false,
+    plugs: false,
+    corbel: false,
+    picture: '',
+    notes: '',
+  };
 
-  constructor(private WorksServices: WorksService){}
-  
+  constructor(private WorksServices: WorksService) {}
+
   @Input() entradaNombre: string = '';
   @Input() entradaTipo: string = '';
   @Input() entradaColor: string = '';
   @Input() entradaImagen: string = '';
   @Input() entradaId: string = '';
 
-  selectWork(id: string):void{
-    this.WorksServices.getWork(id).subscribe({
-      next: (data: Work)=>{
-        this.work = data
-        console.log(this.work);
+  selectWork(id: string): void {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Desea eliminar este trabajo?',
+      showCancelButton: true,
+      confirmButtonText: 'Eliminar',
+      confirmButtonColor: 'red'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.deleteWork(id);
+      } else if (result.isDismissed) {
+        Swal.fire({
+          title: 'No se elimino el trabajo',
+          icon: 'error',
+          timer: 3000,
+          showConfirmButton: false
+        });
       }
-    })
-  }  
-
-  deleteWork(){
-    console.log(this.work.id);
-    
-      this.WorksServices.deleteWork(this.work.id).subscribe({
-      next: ()=>{
-        window.location.reload()
-      }
-    })
+    });
   }
 
-
+  deleteWork(id: string) {
+    this.WorksServices.deleteWork(id).subscribe({
+      next: () => {
+        Swal.fire({
+          title:'Eliminado con exito', 
+          icon: 'success',
+          timer: 3000,
+          showConfirmButton: false
+        });
+        setTimeout(() => {
+          window.location.reload();
+        }, 3000);
+      }
+    });
+  }
 }
