2234100f80042b6a547ef7b494cd7951600a1fe2
Patricio Sirena <patobsirena@gmail.com>
2024-03-14 20:14:16 -0300

    Update photos component to display images from the database.

--- src/app/views/photos/photos.component.ts
+++ src/app/views/photos/photos.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {WorksService} from '../../services/works.service'
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.scss']
 })
-export class PhotosComponent {
+export class PhotosComponent implements OnInit {
+
+  galery: string[] = []
+
+  constructor(private worksService: WorksService){}
+
+  ngOnInit(): void {
+    this.worksService.getWorks().subscribe({
+      next: (data) => {
+        for (let i = 0; i < data.length; i++) {
+          this.galery.push(data[i].picture);
+          console.log(this.galery);
+          
+        }
+      }
+    })
+  }
+
 
 }
